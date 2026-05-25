9c8cbd0c05a566b74b4dfdf6c6c4c6f8c969efdd
FandiMaulanaGacor <pandimaulnaa.com>
2026-05-25 16:30:48 -0400

    update project

--- components/InteractionFlow.tsx
+++ components/InteractionFlow.tsx
@@ -337,7 +337,7 @@ const LoveMeterStep = ({ onComplete }: { onComplete: () => void }) => {
 
 // --- Step 4: Typewriter ---
 const TypewriterStep = ({ onComplete }: { onComplete: () => void }) => {
-    const text = "Semangat kerja ya Ais, tetap jaga kesehatan, jangan dipaksain kalau capek. Jangan sampai sakit, pandi tuh pengen Ais baik-baik aja. pandi always support you in every situation, I’ll always be on your side, I love you so much  ❤️❤️❤️.";
+    const text = "Semangat kerjanya ya Ais, tetap jaga kesehatan, jangan dipaksain kalau capek. Jangan sampai sakit, pandi tuh pengen Ais baik-baik aja. pandi always support you in every situation, I’ll always be on your side, I love you so much  ❤️❤️❤️.";
     const [displayedText, setDisplayedText] = useState("");
     const [isDeleting, setIsDeleting] = useState(false);
 
